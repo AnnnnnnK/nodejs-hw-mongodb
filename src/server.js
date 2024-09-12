559c1a8ca603caf4812937d886d942c1396754b5
AnnnnnnK <129075245+AnnnnnnK@users.noreply.github.com>
2024-09-12 15:44:49 +0200

    add structure

---
+++ src/server.js
@@ -0,0 +1,10 @@
+import express from 'express';
+
+export const setupServer = () => {
+  const app = express();
+  const PORT = 3000;
+
+  app.listen(PORT, () => {
+    console.log(`Server  is running on  port ${PORT}`);
+  });
+};
