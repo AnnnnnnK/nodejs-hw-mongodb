bde878f6f12d7538c4d382ec1473f07b53b001ba
AnnnnnnK <129075245+AnnnnnnK@users.noreply.github.com>
2024-09-13 15:50:23 +0200

    update server.js

--- src/server.js
+++ src/server.js
@@ -32,7 +32,14 @@ export const setupServer = () => {
 
   app.get('/contacts/:contactId', async (req, res) => {
     const { contactId } = req.params;
+
     const contact = await getContactById(contactId);
+
+    if (!contact) {
+      res.status(404).json({
+        message: 'Contact not found',
+      });
+    }
 
     res.status(200).json({
       status: 200,
